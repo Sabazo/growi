6946178bf89043e87c51b6d6574a71670d8b0eda
Shigeru Katahira <s.katahira@siliconlib.com>
2019-11-05 15:07:55 +0900

    yarn add を分けてみる

--- bin/heroku/install-packages.sh
+++ bin/heroku/install-packages.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-yarn add -D $ADDITIONAL_PACKAGES
+export IFS=","
+
+for plugin in $INSTALL_PLUGINS; do
+  yarn add $plugin
+done
+
+for adpack in $ADDITIONAL_PACKAGES; do
+  yarn add -D $adpack
+done
