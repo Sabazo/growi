06018c55f5ccd785e41d9d543f2e1164c29d5abd
Shigeru Katahira <s.katahira@siliconlib.com>
2019-11-06 11:34:20 +0900

    Elasticsearchのポートを9200から443に変更。

--- config/env.dev.js
+++ config/env.dev.js
@@ -4,7 +4,7 @@ module.exports = {
   // MONGO_GRIDFS_TOTAL_LIMIT: 10485760,   // 10MB
   // MATHJAX: 1,
   // NO_CDN: true,
-  ELASTICSEARCH_URI: 'http://localhost:9200/growi',
+  ELASTICSEARCH_URI: 'http://localhost:443/growi',
   HACKMD_URI: 'http://localhost:3010',
   PLUGIN_NAMES_TOBE_LOADED: [
     // 'growi-plugin-lsx',
